767374e5e20e164b1fcf7d91b12d91977075bd68
nourhene <benothmennourhen8@gmail.com>
2025-10-01 14:21:57 +0100

    🛡️ Fix verifyRole middleware and add debug logs for allowed roles

--- server/middlewares/role.js
+++ server/middlewares/role.js
@@ -1,18 +1,18 @@
-// Middleware pour vérifier le rôle basé sur itemtype
 const verifyRole = (...alloweditemtypes) => {
   return (req, res, next) => {
+    console.log('User itemtype:', req.user.itemtype);
+    console.log('Allowed itemtypes:', alloweditemtypes);
+
     if (!req.user) 
       return res.status(401).json({ message: "Unauthorized" });
 
-    // Vérifie si l'itemType de l'utilisateur est dans la liste autorisée
     if (!alloweditemtypes.includes(req.user.itemtype)) {
       return res
         .status(403)
-        .json({ message: "Forbidden: You don't have access to this resource" });
+        .json({ message: "Forbidden: You don\'t have access to this resource" });
     }
 
     next();
   };
 };
-
-module.exports = verifyRole;
+module.exports=verifyRole
