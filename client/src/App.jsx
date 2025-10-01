f0362f3abb8acbf0ad7cca12c43747ad130492fe
nourhene <benothmennourhen8@gmail.com>
2025-10-01 18:33:29 +0100

    🌐 Frontend: Update App routes for dashboards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,26 @@
 // App.jsx
-import {Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+// Dashboards
+import StudentDashboard from "./pages/StudentDashboard";
+import InstructorDashboard from "./pages/InstructorDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+
 function App() {
   return (
     <Routes>
+      {/* Pages publiques */}
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+      {/* Dashboards selon le rôle */}
+      <Route path="/student-dashboard" element={<StudentDashboard />} />
+      <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
     </Routes>
   );
 }
