ff1624b8cac57284cadd3c9caa6bbeac94548f28
nourhene <benothmennourhen8@gmail.com>
2025-10-01 18:34:51 +0100

    ⚡ Frontend: Fix main.jsx BrowserRouter setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,6 +6,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+   
     <BrowserRouter>
       <App />
     </BrowserRouter>
